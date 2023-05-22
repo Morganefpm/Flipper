3074cbfd4a67716bd0955ff0b0fcf74baf47bd52
Bryndye <hugocluzel17@hotmail.fr>
2023-05-22 20:48:32 +0200

    Update gitignore + example code

--- Assets/Script/Bump.cs
+++ Assets/Script/Bump.cs
@@ -2,27 +2,28 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Type
+{
+    type1,
+    type2, type3, type4, type5, type6, type7, type8, type9, type10, type11
+}
 public class Bump : MonoBehaviour
 {
     public float force = 100f;
     public float forceR = 1f;
-    public int scorV = 100;
-    // Start is called before the first frame update
+
     void Start()
     {
         foreach (Collider col in Physics.OverlapSphere(transform.position, forceR))
         {
-            if ( col.GetComponent<Rigidbody>())
-
+            //if ( col.GetComponent<Rigidbody>())
+            //{
+            //    col.GetComponent<Rigidbody>().AddExplosionForce(forceR, transform.position, forceR);
+            //}
+            if (TryGetComponent(out Rigidbody rb))
             {
-                col.GetComponent<Rigidbody>().AddExplosionForce(forceR, transform.position, forceR);
+                rb.AddExplosionForce(forceR, transform.position, forceR);
             }
         }
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
     }
 }
--- Assets/Script/FlipperS.cs
+++ Assets/Script/FlipperS.cs
@@ -1,39 +1,60 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Jobs;
 
+[RequireComponent(typeof(HingeJoint))]
 public class FlipperS : MonoBehaviour
 {
-    public float restPosition = 0f;
-    public float pressedPosition = 45f;
-    public float hitStrenght = 10000f;
-    public float flipperDamper = 150f;
-    HingeJoint hinge;
-    public string inputName;
-    // Start is called before the first frame update
-    void Start()
+    private HingeJoint hinge;
+    private JointSpring spring;
+
+    [Header("Parameters")]
+    
+    [SerializeField] private float restPosition = 0f;
+    [HideInInspector] public float PressedPosition = 45f;
+    [Tooltip("ca sert a r")] public float HitStrenght = 10000f;
+    public float FlipperDamper = 150f;
+    public string InputName = "Horizontal";
+
+    public bool IsOnGround = false;
+
+    //Dictionary<string, int> bouh; LIST AVEC KEY VALUE EN PLUS
+
+    void Awake() // INIT VALUE THIS AVANT START
     {
         hinge = GetComponent<HingeJoint>();
         hinge.useSpring = true;
+
+        spring = new JointSpring();
+        spring.spring = HitStrenght;
+        spring.damper = FlipperDamper;
     }
 
-    // Update is called once per frame
+
     void Update()
+    {
+        IsOnGround = OnGround();
+        PressFlipper();
+        Debug.Log(IsOnGround);
+    }
 
-    { 
-        JointSpring spring = new JointSpring();
-        spring.spring = hitStrenght;
-        spring.damper = flipperDamper;
-
-        if (Input.GetAxis(inputName) == 1)
+    public void PressFlipper()
+    {
+        if (Input.GetAxis(InputName) == 1)
         {
-            spring.targetPosition= pressedPosition;
-         }
-         else
-         {
+            spring.targetPosition = PressedPosition;
+        }
+        else
+        {
             spring.targetPosition = restPosition;
-         }
-         hinge.spring = spring;
-         hinge.useLimits = true;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+    }
+
+    public bool OnGround()
+    {
+        // logic raycast
+        return true;  
     }
 }
